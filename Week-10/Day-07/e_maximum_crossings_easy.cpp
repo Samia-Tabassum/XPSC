f73c71f04f9d7b4529a610dca9438f83846643f7
Samia-Tabassum <163517803+Samia-Tabassum@users.noreply.github.com>
2024-05-31 19:45:56 +0600

    Add files via upload
    added week 10 day 7

---
+++ Week-10/Day-07/e_maximum_crossings_easy.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        pbds<int> p;
+        for(int i=0; i<n; i++)
+        {
+            cin>>a[i];
+        }
+        ll ans=0;
+        for(int i=n-1; i>=0; i--)
+        {
+            ans+=p.order_of_key(a[i]+1);
+            p.insert(a[i]);
+        }
+        cout<<ans<<'\n';
+    }
+    return 0;
+}
