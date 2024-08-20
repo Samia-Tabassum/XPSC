e8a6862d14b38ae788a4885747d2786412d7806d
Samia-Tabassum <163517803+Samia-Tabassum@users.noreply.github.com>
2024-08-20 23:52:12 +0600

    Add files via upload
    added week 17 day 4

---
+++ Week-17/Day-04/a2_segment_tree_for_the_minimum.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+const int maxN=1e5+9, inf=1e9+9;
+int a[maxN], t[4*maxN];
+
+void build(int n, int b, int e)
+{
+    if(b==e)
+    {
+        t[n]=a[b];
+        return;
+    }
+    int mid=(b+e)/2, l=2*n, r=(2*n)+1;
+    build(l, b, mid);
+    build(r, mid+1, e);
+    t[n]=min(t[l], t[r]);
+}
+
+void update(int n, int b, int e, int i, int v)
+{
+    if(i<b || i>e)
+        return;
+    if(b==e)
+    {
+        t[n]=v;
+        return;
+    }
+    int mid=(b+e)/2, l=2*n, r=(2*n)+1;
+    update(l, b, mid, i, v);
+    update(r, mid+1, e, i, v);
+    t[n]=min(t[l], t[r]);
+}
+
+int query(int n, int b, int e, int i, int j)
+{
+    if(e<i || j<b)
+        return inf;
+    if(b>=i && e<=j)
+        return t[n];
+    int mid=(b+e)/2, l=2*n, r=(2*n)+1;
+    return min(query(l, b, mid, i, j), query(r, mid+1, e, i, j));
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int tc=1;
+    //cin>>tc;
+    while(tc--)
+    {
+        int n, m;
+        cin>>n>>m;
+        for(int i=1; i<=n; i++)
+        {
+            cin>>a[i];
+        }
+        build(1, 1, n);
+        while(m--)
+        {
+            int c;
+            cin>>c;
+            if(c==1)
+            {
+                int i, v;
+                cin>>i>>v;
+                i++;
+                update(1, 1, n, i, v);
+            }
+            else
+            {
+                int l, r;
+                cin>>l>>r;
+                l++;
+                cout<<query(1, 1, n, l, r)<<'\n';
+            }
+        }
+    }
+    return 0;
+}
